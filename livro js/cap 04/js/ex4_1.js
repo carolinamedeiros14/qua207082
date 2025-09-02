45137f4b6e02f420a094e35363972997c04f981e
carolina medeiros <carolina.m.silva@aluno.senai.br>
2025-09-01 21:45:16 -0300

    exemplo 4_1 e 4_2

---
+++ livro js/cap 04/js/ex4_1.js
@@ -0,0 +1,30 @@
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("h3")
+const resp2 = document.querySelector("h4")
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const nome = frm.inNome.value
+    const nota1 = Number(frm.inNota1.value)
+    const nota2 = Number(frm.inNota2.value)
+    const media = (nota1 + nota2) /2
+
+    resp1.innerText = `Média das notas: ${media.toFixed(2)}`
+    
+    if(media >= 7){
+        //mensagem de aprovado na cor azul
+        resp2.innerText = `Parabéns ${nome}! Aprovado(a)`
+        resp2.style.color = 'blue'
+
+    } else if(media >=4){
+        //mensagem de atenção fulano
+        resp2.innerText = `Atenção ${nome}! Em exame`
+        resp2.style.color = 'green'
+
+    } else {
+        //mensagem de aprovado na cor vermelha
+        resp2.innerText = `Ops ${nome}! Reprovado(a)`
+        resp2.style.color = 'red'
+    }
+
+})
