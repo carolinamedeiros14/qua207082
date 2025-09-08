0154f5a21d7b1bc34342ac30ca6a04a6190d641f
carolina medeiros <carolina.m.silva@aluno.senai.br>
2025-09-08 20:07:53 -0300

    ex5_1

---
+++ Calculadoida.js
@@ -0,0 +1,9 @@
+let num1 = 10;
+let num2 = 2; 
+let operacao = "*";
+
+if (operacao === "+") { 
+    console.log("Resultado: " + (num1)
+ +num2));
+
+ else if  operacao === "/") { console.log (())}'z'
