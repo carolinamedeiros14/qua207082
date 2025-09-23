fb581ab08252d2d84582a9c669d6a7616cbec40b
carolinamedeiros14 <carolina.m.silva14@aluno.senai.br>
2025-09-22 21:47:47 -0300

    aula 22/09

--- livro-js/cap-06/js/resp6_b.js
+++ livro-js/cap-06/js/resp6_b.js
@@ -4,19 +4,30 @@ const resp2 = document.querySelector("outResp2")
 
 const numeros = []
 frm.addEventListener("submit", (e) => {
-  e.preventDefault()
-  const num = Number(frm.inNumero.value)
-  if(numeros.includes(num) == true){ 
-    alert("Número repetido")
-    return
-  }
+    e.preventDefault()
+    const num = Number(frm.inNumero.value)
+    if (numeros.includes(num) == true) {
+        alert("Número repetido")
+        return
+    }
     numeros.push(num)
-    resp1.innerText = "Numeros: "+numeros.join(", ")
+    resp1.innerText = "Numeros: " + numeros.join(", ")
     frm.inNumero.value = ""
     frm.inNumero.focus()
 })
 
 frm.btVerificar.addEventListener("click", () => {
-let ordem = true
-for (let i = 1; i < numeros.length; i++)
+    let ordem = true
+    for (let i = 1; i < numeros.length; i++) {
+        if (numeros[i - 1] > numeros[i]) {
+            ordem = false
+            break
+        }
+    }
+    if (ordem) {
+        resp2.innerText = "Os números estão em ordem crescente"
+    } else {
+        resp2.innerText = "Os números NÃO estão em ordem crescente"
+    }
+
 })
