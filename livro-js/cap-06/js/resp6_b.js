6a5d638cd60d50017ee0ddfb43f1f9dc59f52cd4
carolinamedeiros14 <carolina.m.silva14@aluno.senai.br>
2025-09-23 21:48:45 -0300

    aula 23/09

--- livro-js/cap-06/js/resp6_b.js
+++ livro-js/cap-06/js/resp6_b.js
@@ -1,33 +1,33 @@
 const frm = document.querySelector("form")
-const resp1 = document.querySelector("outResp1")
-const resp2 = document.querySelector("outResp2")
+const resp1 = document.querySelector("#outResp1")
+const resp2 = document.querySelector("#outResp2")
 
 const numeros = []
-frm.addEventListener("submit", (e) => {
+frm.addEventListener("submit",(e) => {
     e.preventDefault()
-    const num = Number(frm.inNumero.value)
-    if (numeros.includes(num) == true) {
+    const num = frm.inNumero.value
+    if(numeros.includes(num) == true){
         alert("Número repetido")
         return
     }
     numeros.push(num)
-    resp1.innerText = "Numeros: " + numeros.join(", ")
+    resp1.innerText = "Numeros: "+numeros.join(", ")
     frm.inNumero.value = ""
     frm.inNumero.focus()
+
 })
 
 frm.btVerificar.addEventListener("click", () => {
     let ordem = true
-    for (let i = 1; i < numeros.length; i++) {
-        if (numeros[i - 1] > numeros[i]) {
+    for(let i = 1; i < numeros.length; i++){
+        if(numeros[i-1] > numeros[i]){
             ordem = false
             break
         }
     }
-    if (ordem) {
+    if(ordem){
         resp2.innerText = "Os números estão em ordem crescente"
-    } else {
+    }else{
         resp2.innerText = "Os números NÃO estão em ordem crescente"
     }
-
 })
---
+++ livro-js/cap-06/js/resp6_c.js
@@ -0,0 +1,52 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector("pre")
+
+const candidatos = []
+candidatos.push({ nome: "André", acertos: 35 })
+candidatos.push({ nome: "Bianca", acertos: 29 })
+candidatos.push({ nome: "Carlos", acertos: 27 })
+candidatos.push({ nome: "Débora", acertos: 33 })
+candidatos.push({ nome: "Eduardo", acertos: 36 })
+candidatos.push({ nome: "Fernanda", acertos: 28 })
+candidatos.push({ nome: "Giovana", acertos: 31 })
+
+frm.btListar.addEventListener("click", () => {
+    if (candidatos.lenght == 0) {
+        alert("Lista vazia!")
+        return
+    }
+
+    resp.innerText = ""
+    for (const candidato of candidatos) {
+        const { nome, acertos } = candidato
+        resp.innerText += `${nome} - ${acertos} acertos\n`
+    }
+})
+
+frm.btListar.dispatchEvent(new Event("click"))
+
+frm.btAprovados.addEventListener("click", ()=>{
+    const numAcertos = Number(prompt("Número de acertos para aprovação?"))
+    const aprovados = candidatos.filter(candidato => candidato.acertos >= numAcertos)
+    if(aprovados.length == 0){
+        alert("Nenhum aprovado com essa quantidade")
+        return
+    }
+    aprovados.sort((a,b) => b.acertos - a.acertos)
+    resp.innerText = ""
+    for (const candidato of aprovados){
+        const { nome, acertos } = candidato
+        resp.innerText += `${nome} - ${acertos} acertos\n`
+    } 
+})
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const nome = frm.inCandidato.value
+    const acertos = Number(frm.inAcertos.value)
+    candidatos.push({ nome, acertos })
+    frm.inCandidato.value = ""
+    frm.inAcertos.value = ""
+    frm.inCandidato.focus()
+    frm.btListar.dispatchEvent(new Event("click"))
+})
