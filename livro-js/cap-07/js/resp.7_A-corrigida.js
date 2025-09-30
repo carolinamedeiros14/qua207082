4bee4bafeabbf815c041b567e8f6dafa888d6586
carolinamedeiros14 <carolina.m.silva14@aluno.senai.br>
2025-09-30 19:39:50 -0300

    ajuste atividades cap7

--- livro-js/cap-07/js/resp.7_A-corrigida.js
+++ livro-js/cap-07/js/resp.7_A-corrigida.js
@@ -1,49 +1,49 @@
 const frm = document.querySelector("form")
-const resp = document.querySelector("#outResp")
+const resp = document.querySelector("h3")
 
-frm.addEventListener("click", () => {
+frm.addEventListener("submit",(e)=>{
     e.preventDefault()
-    const msg = frm.inMessage.value
+    const msg = frm.inMensagem.value
     let criptografia = ""
-    //compre parcelado
+    // compre parcelado
     // 0123456789012345
-    for (let i = 1; i = < msg.length; i = i + 2) {
+    for(let i = 1; i < msg.length; i = i+2){
         criptografia += msg.charAt(i)
     }
-    for (let i = 0; i = < msg.length; i = i + 2) {
+    for(let i = 0; i < msg.length; i = i+2){
         criptografia += msg.charAt(i)
     }
     resp.innerText = criptografia
 })
 
-frm.btDecriptografar.addEventListener("click", () => {
-    // criptografado > opepreaocmr acld
-    //descriptografado > compre parcelado
-    const criptografia = frm.inMessage.value
-    // metade da palavra
-    let temp = criptografia.substr(0, criptografia.length / 2)
+frm.btDescriptografar.addEventListener("click",()=>{
+    // criptografado    -> opepreaocmr acld
+    // descriptografado -> compre parcelado
+    const criptografia = frm.inMensagem.value
     // array para gravar a msg descriptografada
     const msg = []
+    // metade da palavra
+    let temp = criptografia.substr(0,criptografia.length/2)
     // variável auxiliar para somar os pares
     let aux = 1
     // loop para percorrer somente os pares
-    for (let i = 0; i < temp.length; i++) {
+    for(let i = 0; i < temp.length;i++){
         // colocar na posição pares
         msg[aux] = temp.charAt(i)
-        // incremento do próximo número par
-        aux = + 2
+        // incremento do próximo numero par
+        aux += 2
     }
     // a segunda metade da palavra
     temp = criptografia.substr(criptografia.length/2)
-    // variável auxiliar para somar os pares
+    // variável auxiliar para somar os impares
     aux = 0
-    // loop para percorrer somente os ímpares
+    // loop para percorrer somente os impares
     for(let i = 0; i < temp.length;i++){
-        // colocar na posição ímpar
-        msg [aux] = temp.charAt(i)
-        //incremento do próximo número ímpar
+        // colocar na posição impar
+        msg[aux] = temp.charAt(i)
+        // incremento do próximo numero impar
         aux += 2
     }
-    // escrever mensagem descriptografada
-    resp.innerText. = msg.join("")
+    // escrever mensagemdescriptografada
+    resp.innerText = msg.join("")
 })
--- livro-js/cap-07/js/resp.7_C.js
+++ livro-js/cap-07/js/resp.7_C.js
@@ -2,16 +2,19 @@ const frm = document.querySelector("form")
 const resp1 = document.querySelector("#outResp1")
 const resp2 = document.querySelector("#outResp2")
 
-frm.addEventListener("submit", (e) =>{
+frm.addEventListener("submit",(e) =>{
     e.preventDefault()
     const dataInfracao = frm.inDataInfracao.value.split("-")
     const valorMulta = Number(frm.inValorMulta.value)
-    const dataDesconto = new Date ()
-    dataDesconto.setFullYear(dataInfracao[0])
-    dataDesconto.setMonth(dataInfracao[1]+1)
-    dataDesconto.setDate(dataInfracao[2])
-    dataDesconto = dataDesconto + (1000*60*60*24*90)
-    resp1.innerText = "Data limite para o pagamento com desconto: "
-    resp1.innerText += dataDesconto.getDate().padStar("0",2)
-    resp1.innerText += (dataDesconto.getMonth()+1).padStar("0",2)
+    const dataDesconto = new Date()
+    dataDesconto.setFullYear(Number(dataInfracao[0]))
+    dataDesconto.setMonth(Number(dataInfracao[1])+1)
+    dataDesconto.setDate(Number(dataInfracao[2]))
+    dataDesconto.setDate(Number(dataInfracao[2]))
+    // dataDesconto = dataDesconto + new Date(1000*60*60*24*90)
+    resp1.innerText = "Data Limite para Pagamento com Desconto: "
+    resp1.innerText += dataDesconto.getDate().toString().padStart(2,"0")+"/"
+    resp1.innerText += (dataDesconto.getMonth()+1).toString().padStart(2,"0")+"/"
+    resp1.innerText += dataDesconto.getFullYear()
+    resp2.innerText = "Valor com desconto R$: "+(valorMulta*0.8).toFixed(2)
 })
