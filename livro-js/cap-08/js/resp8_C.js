80928add22dd7074f9470ee390625e5df3ec6b87
carolinamedeiros14 <carolina.m.silva14@aluno.senai.br>
2025-10-08 19:37:55 -0300

    lista de tarefas

--- livro-js/cap-08/js/resp8_C.js
+++ livro-js/cap-08/js/resp8_C.js
@@ -1,29 +1,32 @@
 const frm = document.querySelector("form")
-const resp1 = document.querySelector("outResp1")
-const resp2 = document.querySelector("outResp2")
+const resp1 = document.querySelector("#outResp1")
+const resp2 = document.querySelector("#outResp2")
 
 frm.addEventListener("submit", (e) => {
     e.preventDefault()
+    const valor = Number(frm.inValor.value)
     let desconto = 0
-    if (rbSim.checked) {
-        if (frm.inConvenio.value == "convenio1") {
+    if(rbSim.checked){        
+        if(frm.inConvenio.value == "amigo"){
             desconto = calcularDesconto(valor, 20)
-        } else {
+        }else{
             desconto = calcularDesconto(valor, 50)
         }
-    } else {
+    }else{
         desconto = calcularDesconto(valor, 10)
     }
-    resp.innerText = "Desconto R$: " + desconto.toFixed(2)
-    resp.innerText = "A pagar R$: " + (valor - desconto).toFixed(2)
+    resp1.innerText = "Desconto R$: "+desconto.toFixed(2)
+    resp2.innerText = "A pagar R$: "+(valor-desconto).toFixed(2)
 })
 
-frm.rbSim.addEventListener("click", () => {
-    document.querySelector(#pConveio)
-    className = "exibe"
+frm.rbSim.addEventListener("click",() => {
+    document.querySelector("#pConvenio").className = "exibe"
 })
 
-frm.rbNao.addEventListener("click", () => {
-    document.querySelector(#pConveio)
-    className = "oculta"
+frm.rbNao.addEventListener("click",() => {
+    document.querySelector("#pConvenio").className = "oculta"
 })
+
+function calcularDesconto(valor, taxa){
+    return valor * (taxa/100)
+}
